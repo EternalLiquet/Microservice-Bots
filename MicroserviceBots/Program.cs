7fb1402b22f14938b503ea0fc567df32d43fec74
Aldrin Dominique Natividad <aldmnatividad@gmail.com>
2020-11-15 18:08:23 -0500

    Initial Code Push

---
+++ MicroserviceBots/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MicroserviceBots
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
