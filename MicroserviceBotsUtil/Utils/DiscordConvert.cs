79292fec3809590e1a470b4cc677ffb26d06f486
Aldrin Dominique Natividad <aldmnatividad@gmail.com>
2020-11-16 23:44:11 -0500

    Revert "Revert "Changes""
    This reverts commit db38fe86fa25d3a45a056ace1f87015e5f4ff89b.

---
+++ MicroserviceBotsUtil/Utils/DiscordConvert.cs
@@ -0,0 +1,23 @@
+﻿using Discord.WebSocket;
+using MicroserviceBotsUtil.Entities;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroserviceBotsUtil.Utils
+{
+    public static class DiscordConvert
+    {
+        public static string SerializeObject(SocketMessage message)
+        {
+            var converted = new ConvertedMessage(message);
+            return JsonConvert.SerializeObject(converted, Formatting.None);
+        }
+
+        public static ConvertedMessage DeSerializeObject(string jsonString)
+        {
+            return JsonConvert.DeserializeObject<ConvertedMessage>(jsonString);
+        }
+    }
+}
