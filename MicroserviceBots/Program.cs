db38fe86fa25d3a45a056ace1f87015e5f4ff89b
Aldrin Dominique Natividad <aldmnatividad@gmail.com>
2020-11-16 23:44:01 -0500

    Revert "Changes"
    This reverts commit 4d873e77fec2ba765f5195dc1a7b12563837db4d.

--- MicroserviceBots/Program.cs
+++ MicroserviceBots/Program.cs
@@ -1,47 +1,12 @@
-﻿using BeanBot.Services;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.IO;
-using System.Threading.Tasks;
+﻿using System;
 
-namespace BeanBot
+namespace MicroserviceBots
 {
-    public class Program
+    class Program
     {
-        public static async Task Main(string[] args)
+        static void Main(string[] args)
         {
-            var host = new HostBuilder()
-                .ConfigureHostConfiguration(configHost =>
-                {
-                    configHost.SetBasePath(Directory.GetCurrentDirectory());
-                    configHost.AddJsonFile("Config/hostsettings.json", optional: true);
-                    configHost.AddEnvironmentVariables(prefix: "BOT_");
-                    configHost.AddCommandLine(args);
-                })
-                .ConfigureAppConfiguration((hostContext, configApp) =>
-                {
-                    configApp.AddJsonFile("Config/appsettings.json", optional: true);
-                    configApp.AddJsonFile(
-                        $"Config/appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json",
-                        optional: true);
-                    configApp.AddEnvironmentVariables(prefix: "BOT_");
-                    configApp.AddCommandLine(args);
-                })
-                .ConfigureLogging((hostContext, configLogging) =>
-                {
-                    configLogging.AddConsole();
-                    configLogging.AddDebug();
-                })
-                .ConfigureServices((hostContext, services) => 
-                {
-                    services.AddHostedService<DiscordSocketService>();
-                })
-                .UseConsoleLifetime()
-                .Build();
-            await host.RunAsync();
+            Console.WriteLine("Hello World!");
         }
     }
 }
